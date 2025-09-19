17cd7da8d461fb4b62062c816872fcf50a5d931f
Chris5Flov <c.tend145@gmail.com>
2025-09-18 22:51:25 -0600

    p06 EJ2 y EJ3

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -27,6 +27,81 @@
             }
         }
     ?>
+    <h2>Ejercicio 2</h2>
+    <p>Generación repetitiva de 3 números aleatorios hasta obtener una secuencia</p>
+    <?php
+        function secuencia() {
+            $matriz = [];
+            $iteracion = 0;
+            $numgenerado = 0;
+
+            while (true) {
+                $f = [
+                    rand(1, 999), 
+                    rand(1, 999), 
+                    rand(1, 999)
+                ];
+
+                $numgenerado += 3;
+                $iteracion++;
+
+                $matriz[] = $f;
+
+                if ($f[0] % 2 != 0 && $f[1] % 2 == 0 && $f[2] % 2 != 0) {
+                    break;
+                }
+            }
+
+            return [$matriz, $iteracion, $numgenerado];
+        }
+        list($matriz, $iteracion, $numgenerado) = secuencia();
+        echo "<table border='1' cellpadding='5'>";
+        foreach ($matriz as $f) {
+            echo "<tr><td>" . implode("</td><td>", $f) . "</td></tr>";
+        }
+        echo "</table>";
+        echo "$numgenerado números generados en $iteracion iteraciones realizadas";
+    ?>
+    <h2>Ejercicio 3</h2>
+    <p>Número múltiplo aleatorio con while y do while</p>
+
+    <h2>Con ciclo while</h2>
+    <?php
+    function multiploAleatorioWhile($multiplo){
+        $num = rand(1, 1000);
+        while ($num % $multiplo != 0) {
+            $num = rand(1, 1000);
+        }
+        return $num;
+    }
+
+    if (isset($_GET['multiplo'])) {
+        $multiplo = $_GET['multiplo'];
+        if (is_numeric($multiplo) && $multiplo > 0) {
+            $resultado = multiploAleatorioWhile($multiplo);
+            echo "El primer múltiplo de $multiplo encontrado con while es <strong>$resultado</strong>";
+        }
+    }
+    ?>
+
+    <h2>Con ciclo do while</h2>
+    <?php
+    function multiploAleatorioDoWhile($multiplo){
+        do {
+            $num = rand(1, 1000);
+        } while ($num % $multiplo != 0);
+        return $num;
+    }
+
+    if (isset($_GET['multiplo'])) {
+        $multiplo = $_GET['multiplo'];
+        if (is_numeric($multiplo) && $multiplo > 0) {
+            $resultado = multiploAleatorioDoWhile($multiplo);
+            echo "<br>El primer múltiplo de $multiplo encontrado con do while es <strong>$resultado</strong>";
+        }
+    }
+    ?>
+
 </body>
 </html>
 
