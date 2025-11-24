c8dabe8a6439ae3e004110c6751025a78e9d067a
Chris5Flov <c.tend145@gmail.com>
2025-11-24 14:55:54 -0600

    implementación index y .htaccess

--- practicas/p13/index.php
+++ practicas/p13/index.php
@@ -4,7 +4,13 @@ require 'vendor/autoload.php';
 $app = new Slim\App();
 
 $app->get('/', function ($request, $response, $args ){
+
     $response->getBody()->write("Hola Mundo Slim!!");
+    return $response;
+});
+
+$app->get("/hola[/{nombre}]", function( $request, $response, $args){
+    $response->write("Hola, " . $args["nombre"]);
     return $response;
 });
 
