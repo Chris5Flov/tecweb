7f779ab7ebfc65146fc5ef9ee5dd51c6e47a7481
Chris5Flov <c.tend145@gmail.com>
2025-12-01 18:09:50 -0600

    actualizacion datos myapi

--- actividades/a09/Backend/myapi/DataBase.php
+++ actividades/a09/Backend/myapi/DataBase.php
@@ -1,17 +1,16 @@
 <?php
-namespace Vendor\Composer;
-require_once __DIR__ . '/vendor/autoload.php';
+namespace TECWEB\MYAPI;
+require_once __DIR__ . '/../vendor/autoload.php';
 
 abstract class DataBase {
     protected $conexion;
 
-    public function __construct($db, $user, $pass, $port) {
+    public function __construct($db, $user, $pass) {
         $this->conexion = @mysqli_connect(
             'localhost',
             $user,
             $pass,
-            $db,
-            $port
+            $db
         );
     
         /**
--- actividades/a09/Nat/myapi/Read/product-list.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace Nat\Read;
-    use Vendor\Composer\Products as Products;
-    require_once __DIR__.'/../../Products.php';
-
-    $productos = new Products('marketzone');
-    $productos->list();
-    echo $productos->getData();
-?>
